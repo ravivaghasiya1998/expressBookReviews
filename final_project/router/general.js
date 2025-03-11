f2215ab85165e78578814e63a3a4c472e5fd89ce
Vaghasiya, Ravibhai (EXTERN: IAV GmbH) <ravibhai.vaghasiya@iav.de>
2025-03-11 21:47:54 +0100

    feat: tasks 1-3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,19 +13,34 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4))
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let book_isbn = req.params.isbn ;
+  res.send(books[book_isbn])
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  let authorName = req.params.author;
+  let matchingBooks = [];
+
+  // Iterate over the books object and find books by the given author
+  Object.keys(books).forEach(isbn => {
+      if (books[isbn].author.toLowerCase() === authorName.toLowerCase()) {
+          matchingBooks.push(books[isbn]); 
+      }
+  });
+
+  if (matchingBooks.length > 0) {
+      res.status(200).json(matchingBooks); // Return matching books
+  } else {
+      res.status(404).json({ message: "No books found for this author" }); // Handle no matches
+  }
 });
 
 // Get all books based on title
